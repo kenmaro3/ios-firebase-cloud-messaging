858d44d93e0a0068b946bd43e77b2f712c75e838
kenmaro3 <miha.ken.19@gmail.com>
2023-10-05 15:08:07 +0900

    first commit

---
+++ CloudMessagingIos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CloudMessagingIos
+//
+//  Created by Kentaro Mihara on 2023/08/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
